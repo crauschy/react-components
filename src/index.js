52a3854d0180ace3314e967c1a7b5c6a88ed1f42
Cate Rauschenberger <caterauschy@Cates-MBP.fios-router.home>
2019-04-04 14:41:59 -0400

    start working on component creation and prop use

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import faker from "faker";
+import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => {
+  return (
+    <div className="ui container comments ">
+      <ApprovalCard />
+      <CommentDetail
+        author="Sam"
+        timeAgo="Today at 11:00AM"
+        avatar={faker.image.avatar()}
+        content="Nice blog!"
+      />
+      <CommentDetail
+        author="Alex"
+        timeAgo="Today at 8:28AM"
+        avatar={faker.image.avatar()}
+        content="I dig it!"
+      />
+      <CommentDetail
+        author="Jess"
+        timeAgo="Yesterday at 8:45PM"
+        avatar={faker.image.avatar()}
+        content="Great post!"
+      />
+    </div>
+  );
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.querySelector("#root"));
